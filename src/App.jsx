2aa87092c24ce3b7df90fb8ea2c071a76b340208
AlbertoMNTactico <utm20040077@utma.edu.mx>
2023-11-02 01:07:06 -0600

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function App() {
+  const [cardData, setCardData] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const fetchCardData = async () => {
+      try {
+        const response = await axios.get(`https://api.scryfall.com/cards/named?fuzzy=${searchTerm}`);
+        setCardData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (searchTerm) {
+      fetchCardData();
+    }
+  }, [searchTerm]);
+
   return (
     <div>
-      <h1 className="bg-gradient-to-r from-indigo-400 to-pink-400 bg-clip-text text-8xl font-extrabold text-transparent">
-        We love tailwind 💙
-      </h1>
-      <p className="p-4 pl-2">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-      </p>
+      <h1>Buscar Tarjeta</h1>
+      <input
+        type="text"
+        placeholder="Nombre de la tarjeta"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      {cardData ? (
+        <div>
+          <h2>Información de la tarjeta:</h2>
+          <p>Nombre: {cardData.name}</p>
+          <p>Tipo: {cardData.type_line}</p>
+          {cardData.image_uris && (
+            <div>
+              <h2>Imagen de la tarjeta:</h2>
+              <img src={cardData.image_uris.normal} alt={cardData.name} />
+            </div>
+          )}
+        </div>
+      ) : (
+        <p>Ingresa un nombre de tarjeta para buscar.</p>
+      )}
     </div>
   );
 }
